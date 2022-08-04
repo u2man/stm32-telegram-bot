4da6967a5afc61aebd55bdbcbf781aa27f3783d2
Kang Usman <u_2man@yahoo.co.id>
2022-08-04 13:24:33 +0700

    Add files via upload

---
+++ TouchGFX/generated/fonts/src/Kerning_Roboto_Italic_14_4bpp.cpp
@@ -0,0 +1,29 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_Roboto_Italic_14_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0022, quotedbl], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0027, quotesingle], Kerning dist = -1)
+    { 0x0046, -1 },  // (First char = [0x0046, F], Second char = [0x002C, comma], Kerning dist = -1)
+    { 0x0050, -1 },  // (First char = [0x0050, P], Second char = [0x002C, comma], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x002C, comma], Kerning dist = -1)
+    { 0x0056, -1 },  // (First char = [0x0056, V], Second char = [0x002C, comma], Kerning dist = -1)
+    { 0x0059, -1 },  // (First char = [0x0059, Y], Second char = [0x002C, comma], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x002D, hyphen], Kerning dist = -1)
+    { 0x0046, -1 },  // (First char = [0x0046, F], Second char = [0x002E, period], Kerning dist = -1)
+    { 0x0050, -1 },  // (First char = [0x0050, P], Second char = [0x002E, period], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x002E, period], Kerning dist = -1)
+    { 0x0056, -1 },  // (First char = [0x0056, V], Second char = [0x002E, period], Kerning dist = -1)
+    { 0x0059, -1 },  // (First char = [0x0059, Y], Second char = [0x002E, period], Kerning dist = -1)
+    { 0x0046, -1 },  // (First char = [0x0046, F], Second char = [0x003A, colon], Kerning dist = -1)
+    { 0x0050, -1 },  // (First char = [0x0050, P], Second char = [0x003A, colon], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x003A, colon], Kerning dist = -1)
+    { 0x0056, -1 },  // (First char = [0x0056, V], Second char = [0x003A, colon], Kerning dist = -1)
+    { 0x0059, -1 },  // (First char = [0x0059, Y], Second char = [0x003A, colon], Kerning dist = -1)
+    { 0x0050, -1 },  // (First char = [0x0050, P], Second char = [0x0041, A], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0054, T], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0056, V], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0057, W], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x0059, Y], Kerning dist = -1)
+    { 0x004C, -1 },  // (First char = [0x004C, L], Second char = [0x201D, quotedblright], Kerning dist = -1)
+};
---
+++ TouchGFX/generated/fonts/src/Kerning_Roboto_Regular_24_4bpp.cpp
@@ -0,0 +1,6 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_Roboto_Regular_24_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x003F, question], Kerning dist = -1)
+};
